3d853318345921dcc737d0a7ba88ba0179c463e5
MacBook Pro <bazdyrev1@gmail.com>
2022-08-02 15:02:38 +0300

    finaly vers.

--- index.ts
+++ index.ts
@@ -1,7 +1,5 @@
-interface Ifighter {
-  
-    
-    getWins():number,
+interface IFighter {
+    getWins(): number,
     getLosses(): number,
     getName(): string,
     getDamage(): number,
@@ -15,154 +13,136 @@ interface Ifighter {
     dealDamage(dam: number): void,
     hit(hero: object): void,
     hitProbability(): boolean,
-    attack(hero: object): void
-
+    attack(hero: object): void,
+}
+interface IPropsObj {
+    name: string;
+    damage: number;
+    hp: number;
+    strenght: number;
+    agility: number;
 }
 
-
-
-
-class Fighter implements Ifighter{
-   name: string;
+class Fighter implements IFighter {
+    name: string;
     damage: number;
     hp: number;
     strenght: number;
     agility: number;
     wins: number;
     losses: number;
-    startedHp: number;
 
-    constructor(name: string,
-        damage: number,
-        hp: number,
-        strenght: number,
-        agility: number,
-        wins: number,
-        losses: number,
-        startedHp: number,
-        ) {
-        this.name = name;
-        this.damage = damage;  
-        this.hp = hp;
-        this.startedHp = startedHp;
-        this.strenght = strenght;
-        this.agility =agility;
-        this.wins = wins;
-        this.losses = losses;
+    constructor(obj: IPropsObj) {
+        this.name = obj.name;
+        this.damage = obj.damage;
+        this.hp = obj.hp;
+        this.strenght = obj.strenght;
+        this.agility = obj.agility;
+        this.wins = 0;
+        this.losses = 0;
     }
 
-    getWins(){
+    getWins() {
         return this.wins;
     };
-
-    getLosses(){
+    getLosses() {
         return this.losses;
     };
-    getName(){
+    getName() {
         return this.name
     };
-    getDamage(){
+    getDamage() {
         return this.damage;
     };
-    getHealth(){
+    getHealth() {
         return this.hp;
     };
-    getStrenght(){
+    getStrenght() {
         return this.strenght;
     };
-    getAgility(){
+    getAgility() {
         return this.agility;
     };
-    addLoss(){
+    addLoss() {
         this.losses = this.getLosses() + 1;
-    }
-    addWin(){
+    };
+    addWin() {
         this.wins = this.getWins() + 1;
-    }
+    };
     logCombatHistory() {
         console.log(`Name : ${this.getName()}, Wins: ${this.getWins()}, Losses: ${this.getLosses()} `);
-    }   
-    heal(value: number){
-        this.getHealth() + value > this.startedHp ? this.hp = this.startedHp : this.hp = this.getHealth() + value;
-    }
-    dealDamage(dam:  number){
+    };
+    heal(value: number) {
+        this.getHealth() + value > this.hp ? this.hp = value : this.hp = this.getHealth() + value;
+    };
+    dealDamage(dam: number) {
         this.getHealth() >= dam ? this.hp = this.getHealth() - dam : this.hp = 0
-    }
-    hit(hero: Ifighter){
+    };
+    hit(hero: IFighter) {
         hero.dealDamage(this.getDamage());
         console.log(`${this.getName()} makes ${this.getDamage()} damage ${hero.getName()}`);
-    }
+    };
     hitProbability() {
         const hitProbability = 100 - (this.strenght + this.agility);
         const hit = Math.random() <= hitProbability / 100;
         return hit;
     };
-    attack(hero: Ifighter) {
+    attack(hero: IFighter) {
         this.hitProbability() ? this.hit(hero) : console.log(`${this.name} attack missed`);
     };
-    
-
 }
 
+const myFighter: IFighter = new Fighter({
+    name: 'Maximus',
+    damage: 25,
+    hp: 100,
+    strenght: 30,
+    agility: 25,
+});
 
-const myFighter: Ifighter = new Fighter(
-   'Maximus',
-   25,
-   100,
-    30,
-    25,
-    0,
-    0,
-    100
-    
-);
+const opponentFighter: IFighter = new Fighter({
+    name: 'Commodus',
+    damage: 25,
+    hp: 90,
+    strenght: 25,
+    agility: 20,
+});
 
-const opponentFighter: Ifighter = new Fighter(
-   'Commodus',
-    25,
-    90,
-    25,
-    20,
-    0,
-    0,
-    100
-);
+const condition: boolean = myFighter.getHealth() > 0 && opponentFighter.getHealth() > 0;
+const isDeadHero: boolean = myFighter.getHealth() === 0;
+const isDeadOpponent: boolean = opponentFighter.getHealth() === 0;
 
-function battle(myFighter: Ifighter, opponentFighter: Ifighter): void {
-    if (myFighter.getHealth() === 0
-      || opponentFighter.getHealth() === 0) {
-      myFighter.getHealth() === 0 ? console.log(`${myFighter.getName()} is dead and can\`t fight.`) : console.log(`${opponentFighter.getName()} is dead and can\`t fight.`);
+function battle(myFighter: IFighter, opponentFighter: IFighter): void {
+    if (isDeadHero || isDeadOpponent) {
+        myFighter.getHealth() === 0 ? console.log(`${myFighter.getName()} is dead and can\`t fight.`) : console.log(`${opponentFighter.getName()} is dead and can\`t fight.`);
     } else {
-      while (
-        myFighter.getHealth() > 0
-        && opponentFighter.getHealth() > 0
-      ) {
-        myFighter.attack(opponentFighter);
-        opponentFighter.attack(myFighter);
-      }
-  
-      switch (true) {
-        case myFighter.getHealth() > opponentFighter.getHealth():
-          console.log(`${myFighter.getName()} has won!`);
-          myFighter.addWin();
-          opponentFighter.addLoss();
-          break;
-        case opponentFighter.getHealth() > myFighter.getHealth():
-          console.log(`${opponentFighter.getName()} has won!`);
-          opponentFighter.addWin();
-          myFighter.addLoss();
-          break;
-        default:
-          myFighter.addLoss();
-          opponentFighter.addLoss();
-          console.log('There are no winners! Both opponents fell in battle!');
-          break;
-      }
+        while (condition) {
+            myFighter.attack(opponentFighter);
+            opponentFighter.attack(myFighter);
+        }
+
+        switch (true) {
+            case myFighter.getHealth() > opponentFighter.getHealth():
+                console.log(`${myFighter.getName()} has won!`);
+                myFighter.addWin();
+                opponentFighter.addLoss();
+                break;
+            case opponentFighter.getHealth() > myFighter.getHealth():
+                console.log(`${opponentFighter.getName()} has won!`);
+                opponentFighter.addWin();
+                myFighter.addLoss();
+                break;
+            default:
+                myFighter.addLoss();
+                opponentFighter.addLoss();
+                console.log('There are no winners! Both opponents fell in battle!');
+                break;
+        }
     }
-  }
+}
 
 console.log(myFighter);
 console.log(opponentFighter);
 console.log(battle(myFighter, opponentFighter))
 
-export{}
+export { }
